a8096dda8f16fbc1fe05e7e898fe4aa4d280046b
Oleg Berezhnoy <bereg2k@gmail.com>
2019-08-13 15:25:00 +0300

    Task 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,9 @@
-/** Задание 1: Переменные
- *
- * 1) Создайте переменную со значением null типа Any?
- * 2) Переопределите значение ранее созданной переменной на любую цифру
- * 3) Создайте переменную, не указывая её тип и присвойте ей значение из ранее созданной переменной деленную на 2,5
- * 4) Напишите в комментарии какого типа последняя переменная
+/** Задание 2: Hello Aplana
+ * Создайте две переменные типа String, содержащие "hello" и "aplana"
+ * И выведите их в консоль так что бы получилось: hello aplana
  */
 fun main(args: Array<String>) {
-    var someVar: Any? = null
-    someVar = 7
-    val anotherVar = someVar / 2.5
-    print("anotherVar = $anotherVar and it is of ${anotherVar.javaClass} type")
+    val hello = "hello"
+    val aplana = "aplana"
+    print("$hello $aplana")
 }
